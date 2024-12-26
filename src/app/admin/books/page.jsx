663b2042a6ca4e69b47130d682018efaca408725
Your Name <your_email@example.com>
2024-12-26 09:47:02 +0545

    try

--- src/app/admin/books/page.jsx
+++ src/app/admin/books/page.jsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 
-export default function Books(){
+export default function Books() {
     return(
         <div className="flex justify-between">
-            <h1>All Books</h1>
+            <h1>All Books </h1>
             <Button >
                 Add Book <Plus />
 
