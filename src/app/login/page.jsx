6038266c0877cae03b3d0f0c67ed25bd77e42fef
Your Name <your_email@example.com>
2024-12-27 09:16:09 +0545

    login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -10,13 +10,13 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
+
   FormField,
   FormItem,
-  FormLabel,
+
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input"
+// import { Input } from "@/components/ui/input"
 import { Card } from "@/components/ui/card";
 import {   Eye, EyeOff, User,Lock } from "lucide-react";
 
