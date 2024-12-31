f3fe5d07648710468e524834476d569e7bea4b87
EZLIZZ <alishashrestha7777@gmail.com>
2024-12-31 10:01:05 +0545

    search filter

--- src/app/admin/books/_components/BookList.jsx
+++ src/app/admin/books/_components/BookList.jsx
@@ -1,6 +1,9 @@
+"use client"
 import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Pencil, Trash } from "lucide-react";
+import { useState } from "react";
 
 const Books = [
   {
@@ -46,9 +49,23 @@ const Books = [
 ];
 
 export default function BookList() {
+  const [searchBook,setSearchBook] = useState("");
+  const filterBooks = Books.filter((book)=>
+  book.name.toLowerCase().includes(searchBook.toLowerCase())||
+book.author.toLowerCase().includes(searchBook.toLowerCase()));
   return (
     <div>
+      <div className="w-full flex justify-end">
+      <Input
+        type="text"
+        placeholder="Search for book or author"
+        value={searchBook}
+        onChange={(e)=> setSearchBook(e.target.value)}
+        className="mt-5 mx-5 lg:w-[15%] w-full border-primary"
+        />
+        </div>
       <Card className="m-5 px-5 bg-transparent ">
+        
         <Table>
           <TableHeader>
             <TableRow>
@@ -60,7 +77,7 @@ export default function BookList() {
             </TableRow>
           </TableHeader>
           <TableBody>
-            {Books.map((books)=>(
+            {filterBooks.map((books)=>(
               <TableRow key={books.id}>
                 <TableCell><div className="rounded-lg w-16 h-24 overflow-hidden"><img src={books.img} className="w-full h-full object-contain" /></div></TableCell>
                 <TableCell><h1 className="font-semibold text-base text-primary">{books.name}</h1> {books.author}</TableCell>
