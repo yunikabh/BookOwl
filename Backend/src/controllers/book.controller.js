81bbee421533ec50237f36b2384d76b2ed7d88f0
yunikabh <yunika.211448@ncit.edu.np>
2025-01-25 00:11:31 +0545

    Category filter

--- Backend/src/controllers/book.controller.js
+++ Backend/src/controllers/book.controller.js
@@ -1,5 +1,5 @@
 import express from "express";
-// import User from "../models/user.model.js";
+import User from "../models/user.model.js";
 import Book from "../models/book.model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/apiError.js";
@@ -346,7 +346,7 @@ try{
       $sort: { totalBooks: -1 }, // Sort categories by the number of books in descending order
   },
 ]);
-    res.status(200).json(new ApiResponse(200,{totalAuthors,totalUsers,categoryStats},"Books by category statistics fetched successfully"))
+    res.status(200).json(new ApiResponse(200,{totalBooks,totalUsers,categoryStats},"Books by category statistics fetched successfully"))
 
 } catch (error) {
   throw new  ApiError(400,"Smthg went wrong");
